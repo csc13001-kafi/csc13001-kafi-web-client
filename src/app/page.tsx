0e1b8d6ba189ce67073f185e16f353b837fa15db
Tường Vi <148709510+phthtgvi@users.noreply.github.com>
2025-03-09 15:32:30 +0700

    fix: remove git warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-<<<<<<< Updated upstream
 import { HomePage } from "@/components/pages/home-page";
-=======
-import HomePage from "@/components/pages/home-page";
 import Menu from "@/components/pages/menu";
->>>>>>> Stashed changes
 
 export default function Home() {
   return <HomePage />;
