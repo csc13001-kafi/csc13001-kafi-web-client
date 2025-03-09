6cb5c846da0f56351b07366e23a7598d20b4a8a3
Tường Vi <148709510+phthtgvi@users.noreply.github.com>
2025-03-09 15:19:55 +0700

    feat: add menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { HomePage } from "@/components/pages/home-page";
+import Menu from "@/components/pages/menu";
 
 export default function Home() {
-  return <HomePage />;
+  return <Menu />;
 }
