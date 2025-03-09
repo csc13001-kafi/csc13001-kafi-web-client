f3bad87d7dce82bed1d751bcf6d0bd65577d32fc
Tường Vi <148709510+phthtgvi@users.noreply.github.com>
2025-03-09 15:31:53 +0700

    refactor: rearrange components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import { HomePage } from "@/components/pages/home-page";
+=======
+import HomePage from "@/components/pages/home-page";
+import Menu from "@/components/pages/menu";
+>>>>>>> Stashed changes
 
 export default function Home() {
   return <HomePage />;
