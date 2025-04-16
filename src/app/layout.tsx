cf7980d9bdbe0ed2eb48db2d4b9c40ff5899f55d
ntkwan <npquan2704@gmail.com>
2025-04-16 10:58:10 +0700

    feat: integrate customer feedback endpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { CounterStoreProvider } from "@/providers/counter-store-provider";
-import { AuthProvider } from "@/providers/auth-provider";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { CounterStoreProvider } from '@/providers/counter-store-provider';
+import { AuthProvider } from '@/providers/auth-provider';
+import { Toaster } from 'react-hot-toast';
 
-const inter = Inter({ 
-  subsets: ["latin"],
-  variable: "--font-inter" 
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
-  title: "Kafi - Coffee Shop",
-  description: "Kafi coffee shop website",
+    title: 'Kafi - Coffee Shop',
+    description: 'Kafi coffee shop website',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
-        <AuthProvider>
-          <CounterStoreProvider>{children}</CounterStoreProvider>
-        </AuthProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.variable} antialiased`}>
+                <Toaster position="top-center" />
+                <AuthProvider>
+                    <CounterStoreProvider>{children}</CounterStoreProvider>
+                </AuthProvider>
+            </body>
+        </html>
+    );
 }
